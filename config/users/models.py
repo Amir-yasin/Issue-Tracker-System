23a105b91bf4864030e06988adf99586cad71399
amir yasin <amiryasin3262@gmail.com>
2026-02-24 16:17:50 +0300

    first commit

---
+++ config/users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('ADMIN', 'Admin'),
+        ('SUPPORT', 'Support'),
+        ('MERCHANT', 'Merchant'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    phone = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.username
